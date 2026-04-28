eb14d3ea5d4d62f7202e65d86068305628b0b2d8
vrushabh <vrushabhzingade7@gmail.com>
2026-04-28 16:11:39 +0530

    auto create superuser

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+
 pip install -r requirements.txt
+
 python manage.py migrate
+
 python manage.py collectstatic --noinput
+
+# Create superuser automatically
+echo "from django.contrib.auth.models import User;
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin','admin@gmail.com','admin123')" | python manage.py shell
