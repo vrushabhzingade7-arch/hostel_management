fb4137d3bd3ef5464141bf00634a876f86e1262f
vrushabh <vrushabhzingade7@gmail.com>
2026-04-29 12:57:51 +0530

    Switched to PostgreSQL

--- hostel_management/settings.py
+++ hostel_management/settings.py
@@ -68,7 +68,9 @@ WSGI_APPLICATION = 'hostel_management.wsgi.application'
 # ================= DATABASE =================
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3'
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
