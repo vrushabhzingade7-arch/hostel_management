f429b0c00a58251be3e5b2c7b59160c7a87cff9f
vrushabh <vrushabhzingade7@gmail.com>
2026-04-29 16:45:33 +0530

    added role users auto creation

--- build.sh
+++ build.sh
@@ -6,10 +6,25 @@ python manage.py migrate
 
 python manage.py collectstatic --noinput
 
-# Create superuser safely
+# Create users (admin + roles)
 echo "
 from django.contrib.auth import get_user_model
 User = get_user_model()
+
+# ---------------- ADMIN ----------------
 if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin','admin@gmail.com','admin123')
+    User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')
+
+# ---------------- ROLE USERS ----------------
+users = [
+    ('cc_user', 'Pass@123'),
+    ('hod_user', 'CSEHod@124'),
+    ('rector_user', 'Rector@124'),
+]
+
+for username, password in users:
+    if not User.objects.filter(username=username).exists():
+        u = User.objects.create_user(username=username, password=password)
+        u.is_staff = True
+        u.save()
 " | python manage.py shell
