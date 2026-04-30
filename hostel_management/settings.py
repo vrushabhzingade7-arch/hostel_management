2fd0423725d2ae7eb94ff9de6187ece50c132bf5
vrushabh <vrushabhzingade7@gmail.com>
2026-04-30 15:29:17 +0530

    Fixed email config

--- hostel_management/settings.py
+++ hostel_management/settings.py
@@ -113,11 +113,14 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 # ================= EMAIL CONFIG =================
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'vrushabhzingade@gmail.com'
-EMAIL_HOST_PASSWORD = 'mzat dmab vmpl rwlp'  # ⚠️ NOT your normal Gmail password
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
