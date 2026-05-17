bb0aa26b9905ba95049efb913ad643262e32b770
vrushabh <vrushabhzingade7@gmail.com>
2026-05-17 18:56:42 +0530

    Fixed email settings

--- hostel_management/settings.py
+++ hostel_management/settings.py
@@ -119,12 +119,16 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
+
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+# IMPORTANT
+SERVER_EMAIL = EMAIL_HOST_USER
 
 # ================= LOGIN =================
 LOGIN_URL = 'admin_login'
